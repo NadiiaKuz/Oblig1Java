fb60a27d10b2771ce30c3d2d01390116c6ff89c1
NadiiaKuz <NadinK1603@gmail.com>
2025-01-10 12:51:58 +0100

    added oblig1

---
+++ src/oppgave2_2/Main.java
@@ -0,0 +1,11 @@
+package oppgave2_2;
+
+public class Main {
+    public static void main(String[] args) {
+        double gravity = 16.5;
+
+        System.out.println("Skriv inn din vekt: ");
+
+
+    }
+}
