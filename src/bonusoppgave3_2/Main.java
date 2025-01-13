a49ce753109c4495d90468d84990f98174b9f783
NadiiaKuz <NadinK1603@gmail.com>
2025-01-13 15:26:45 +0100

    added oppgave3_2

---
+++ src/bonusoppgave3_2/Main.java
@@ -0,0 +1,37 @@
+package bonusoppgave3_2;
+
+import oppgave2_3.Planet;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("Skriv inn navn på planeten: ");
+            String inputName = scanner.nextLine();
+
+            System.out.println("Skriv inn radius til planeten: ");
+            double inputRadius = scanner.nextDouble();
+
+            System.out.println("Skriv inn massen til planeten: ");
+            double inputMass = scanner.nextDouble();
+            scanner.nextLine();
+
+            Planet planet = new Planet(inputName, inputRadius, inputMass);
+
+            System.out.printf("Planeten %s har en radius på %s km og en masse på %s kg", planet.getName(), planet.getRadius(), planet.getMass());
+
+            System.out.println("\n--------------\n");
+
+            System.out.println("Vil du fortsette? Y/N: ");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("y")){
+                break;
+            }
+        }
+    }
+}
